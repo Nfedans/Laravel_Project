106f51750cd5ec903b7bd46e2fffb2997391daec
TheBogman02 <92158821+TheBogman02@users.noreply.github.com>
2022-05-01 12:33:05 +0100

    github login fix( resolved merge conflicts)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -82,11 +82,7 @@
                         class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
                             {{ __('Sign-in with Google') }}
             </a> 
-<<<<<<< HEAD
             <a href="/redirectgithub"
-=======
-            <a href="/redirectGit"
->>>>>>> 81f696274858e160206ca4fda6e7c7a589fe5a54
                         class="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900">
                             {{ __('Sign-in with Github') }}
             </a> 
